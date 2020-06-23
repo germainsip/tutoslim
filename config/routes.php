59070e93a5057d0b298ff576b76052798b2854c5
Germain SIPIERE <gsipiere@gmail.com>
2020-06-23 11:36:20 +0200

    test route

--- config/routes.php
+++ config/routes.php
@@ -16,6 +16,8 @@ return function (App $app) {
 
     $app->get('/', \App\Action\HomeAction::class);
     $app->get('/users/{id}', \App\Action\UserReadAction::class);
+    $app->get('/user/{id}', \App\Action\UserReadAction::class);
+
     $app->post('/users', \App\Action\UserCreateAction::class);
 
 };
